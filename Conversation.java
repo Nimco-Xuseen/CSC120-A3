23987751475063768b81481e86ff1961b67ce0fb
Nimco-Xuseen <nxuseen@smith.edu>
2024-09-25 19:42:06 -0400

    commiting

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,96 @@
-class Conversation {
+import java.util.Scanner;
+import java.util.Random; 
 
-  public static void main(String[] arguments) {
+public class Conversation {
+
+  public static void main(String[] args) {
     // You will start the conversation here.
+    Scanner input = new Scanner(System.in);
+
+    // Asking user for the number of conversation rounds
+    System.out.print("How many rounds?");
+    int rounds = input.nextInt();
+    input.nextLine();
+
+    //print initial greeting
+    System.out.println("Hi there! Mind sharing what is on your mind?");
+
+    //Storung the conversation transcript
+    StringBuilder transcript = new StringBuilder();
+    transcript.append("Hi there! Mind sharing what is on your mind?\n");
+
+    //responses
+    String[] cannedResponses = {
+      "Interesting, tell me more.",
+      "I'm sorry, I didn't understand that",
+      "It will be alright!",
+      "Thank you for sharing!",
+      "Goodbye!"
+    };
+
+    // number of rounds
+    for (int i = 0; i< rounds; i++ ) {
+      // Get user unput
+      String userInput = input.nextLine();
+      transcript.append(userInput).append("\n");
+
+      //Mirror response logic
+      String response = generateResponse(userInput, cannedResponses);
+      System.out.println(response);
+      transcript.append(response).append("\n");
+    }
+
+    //Goodbye message 
+    System.out.println("Okay, Have a good day!");
+    transcript.append("Okay, Have a good day!\n");
+
+    // Print transcript
+    System.out.println("\nTRANSCRIPT:");
+    System.out.println(transcript.toString());
+
+    input.close();
+
+  }
+
+  // Function to generate the chatbot's response
+  public static String generateResponse(String userInput, String[] cannedResponses) {
+    // Words to be mirrored
+    String[][] mirrorWords = {
+        {"I am", "You are"},
+        {"me", "You"},
+        {"am", "are"},
+        {"You", "I"},
+        {"my", "your"},
+        {"your", "my"}
+    };
+
+    //split user input into words
+    String[] words = userInput.split("\\s+");
+
+    //Checking if we need to mirror any words
+    boolean mirrored = false; 
+    for (int i=0; i < words.length; i++){
+      for (String[] pair : mirrorWords) {
+        if (words[i].equalsIgnoreCase(pair[0])) {
+          words[i] = pair[1];
+          mirrored = true;
+        } else if (words[i].equalsIgnoreCase(pair[1])) {
+            words[i] = pair[0];
+            mirrored = true;
+
+        }
+      }
+    }
+
+    
+    // If mirrored, return the mirrored sentence
+    if (mirrored) {
+      return String.join(" ", words) + "?";
+  } else {
+      // Otherwise, return a random canned response
+      Random rand = new Random();
+      return cannedResponses[rand.nextInt(cannedResponses.length)];
   }
 }
+}
+
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+- Liyana and Sofia
 - Which session(s) of TA / office hours did you attend?
+  I wasn't able to attend neither of those but I did consult the teachers via chat. 
 - What are your initial impressions of Java? 
+    Java is not that bad, but technically this is my first time ever doing coding so my experience might be different from other people's. I am taking it day by day.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+- Many languages support both object-oriented (OO) and procedural programming styles. Java, for instance, is explicitly object-oriented, whereas languages like Python can accommodate both OO and procedural approaches.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+- I was having a hard time figuring out how to add the question mark at the first response and also having an uppercase when it responds. I have got it all figured out after sometime. Ask for help whenever you get stuck at something. 
